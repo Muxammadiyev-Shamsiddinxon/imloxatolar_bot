acde1b26b274b3125b17b9a4534612c9eb10e3cd
Shamsiddinxon <100491367+Muxammadiyev-Shamsiddinxon@users.noreply.github.com>
2022-07-15 17:19:25 +0500

    5280188027
    5280188027

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -25,4 +25,4 @@ async def bot_start(message: types.Message):
     # Adminga xabar beramiz
     count = db.count_users()[0]
     msg = f"ismi👉 <b>{message.from_user.full_name}</b>   id👉 <b>{message.from_user.id}</b>  bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    await bot.send_message(chat_id=ADMINS[0], text=msg)
+    await bot.send_message(chat_id="5280188027", text=msg)
