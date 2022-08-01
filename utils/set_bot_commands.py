8bed7e310fbe67a4c22a8dd3f8dfc3a228da8c0e
Shamsiddinxon <100491367+Muxammadiyev-Shamsiddinxon@users.noreply.github.com>
2022-08-01 14:16:22 +0500

    yangilandi
    yangilandi

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,9 +6,7 @@ async def set_default_commands(dp):
         [
             types.BotCommand("start", "Botni ishga tushurish"),
             types.BotCommand("help", "Yordam"),
-            types.BotCommand("obunachilar", "Barcha foydalanuvchilar soni"),
-            types.BotCommand("reklama", "Barcha foydalanuvchilarga reklama yuborish"),
-            types.BotCommand("baza_tozalash", "Bazadagi barcha ma'lumotlarni tozalaydi")
+            types.BotCommand("admin_commands", "Commandalar")
 
         ]
     )
